8008a822e011b89a0523388329dc1cf2ab120686
Abhi Jadhav <124767136+i-dontno@users.noreply.github.com>
2023-10-14 10:58:49 +0530

    Create Pr07Primeupto.c

---
+++ Pr07Primeupto.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+ printf("Prime numbers from 1 to 100:\n");
+
+ for (int num = 2; num <= 100; num++) {
+
+ int isPrime = 1;
+
+ for (int i = 2; i * i <= num; i++) {
+
+ if (num % i == 0) {
+
+ isPrime = 0;
+
+ break;
+
+ }
+
+ }
+
+ if (isPrime) {
+
+ printf("%d ", num);
+
+ }
+
+ }
+
+ printf("\n");
+
+ return 0;
+
+}
