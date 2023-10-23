86ba84a34a44c3fcb92788beeb7f0325e4daaf85
Abhi Jadhav <124767136+i-dontno@users.noreply.github.com>
2023-10-23 12:19:02 +0530

    Create MatrixAdd.c

---
+++ MatrixAdd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+
+ int a[3][3], b[3][3], sum[3][3], i, j;
+
+ printf("\nEnter elements of 1st matrix:\n");
+
+ for (i = 0; i < 3; ++i)
+
+ for (j = 0; j < 3; ++j) {
+
+ printf("Enter element a%d%d: ", i + 1, j + 1);
+
+ scanf("%d", &a[i][j]);
+
+ }
+
+ printf("Enter elements of 2nd matrix:\n");
+
+ for (i = 0; i < 3; ++i)
+
+ for (j = 0; j < 3; ++j) {
+printf("Enter element b%d%d: ", i + 1, j + 1);
+
+ scanf("%d", &b[i][j]);
+
+ }
+
+ // adding two matrices
+
+ for (i = 0; i < 3; ++i)
+
+ for (j = 0; j < 3; ++j) {
+
+ sum[i][j] = a[i][j] + b[i][j];
+
+ }
+
+ // printing the result
+
+ printf("\nSum of two matrices: \n");
+
+ for (i = 0; i < 3; ++i)
+
+ for (j = 0; j < 3; ++j) {
+
+ printf("%d ", sum[i][j]);
+
+ if (j == 3 - 1) {
+
+ printf("\n\n");
+
+ }
+
+ }
+
+ return 0;
+
+}
